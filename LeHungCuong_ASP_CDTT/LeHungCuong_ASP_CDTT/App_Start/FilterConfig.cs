ea3823aee4259cada8746b530e9892404694550a
LordCuong <cuongle0838723011@gmail.com>
2025-07-29 14:15:42 +0700

    up lan 1 29/7/25

---
+++ LeHungCuong_ASP_CDTT/LeHungCuong_ASP_CDTT/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LeHungCuong_ASP_CDTT
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LeHungCuong_ASP_CDTT/LeHungCuong_ASP_CDTT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeHungCuong_ASP_CDTT.MvcApplication" Language="C#" %>
