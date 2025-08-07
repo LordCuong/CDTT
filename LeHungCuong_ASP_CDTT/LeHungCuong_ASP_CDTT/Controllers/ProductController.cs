2d60ac76558cc959bc62a2d5d9ac8b7a3afd8c9f
LordCuong <cuongle0838723011@gmail.com>
2025-08-07 16:29:52 +0700

    update on 7/8

---
+++ LeHungCuong_ASP_CDTT/LeHungCuong_ASP_CDTT/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeHungCuong_ASP_CDTT.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        public ActionResult ListingProduct()
+        {
+            return View();
+        }
+    }
+}
